d52a9521e4cab5458dc15d7f88852bef441a6e55
Juseok Oh <juseoko2@illinois.edu>
2023-03-30 14:07:21 -0500

    3/30

--- TEIcommands.R
+++ TEIcommands.R
@@ -1,6 +1,10 @@
 library(xml2)
 doc <- read_xml("JaneEyre.tei")
 xml_ns_strip(doc)
-xml_find_all(doc, "//lg")
+xml_find_all(doc, "//head")
+list <- xml_find_all(doc, "//lg")
+for (i in list)
+  return(head)
 
 
+xml_structure(doc)
