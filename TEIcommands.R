b9b0c7cd5b765c405644df6e525be051308918b0
Carlos Conley <carlosconley@icloud.com>
2023-03-30 13:54:10 -0500

    Add student code

--- TEIcommands.R
+++ TEIcommands.R
@@ -1,6 +1,30 @@
 library(xml2)
 doc <- read_xml("JaneEyre.tei")
 xml_ns_strip(doc)
-xml_find_all(doc, "//lg")
+
+verses <- xml_find_all(doc, "//lg")
+
+# We use xml children to get all of the children on the same level as the chapters
+body.v <- xml_children(xml_children(xml_children(doc)[2])[1])[10]
+
+# Chapter 1 starts at 5, so we get all children 5:5+38
+chapters.v <- xml_children(body.v)[5:43]
+
+print(chapters.v)
+
+verse.chapters <- c()
+i <- 1 # We start at chapter 1
+
+# We try to loop through every chapter to check each chapter for the 
+# existence of a <lg> tag, and print the chapter number if we do
+for (chapter in chapters.v) {
+  if (length(xml_find_all(chapter, "//lg")) > 0) { # this expression doesn't work as expected
+    print(i)
+  }
+  i <- i + 1
+}
+
+
+
 
 
