bb0caed8933491b7dc18c1f360bb94fa8b0e3ec6
Richard Liu <richardliu@R-MBP-M1.local>
2023-03-30 16:17:41 -0500

    Found the second parent of the node, which is the chapter, and found its id attribute.

--- TEIcommands.R
+++ TEIcommands.R
@@ -1,6 +1,11 @@
 library(xml2)
 doc <- read_xml("JaneEyre.tei")
 xml_ns_strip(doc)
-xml_find_all(doc, "//lg")
+v <- xml_find_all(doc, "//lg")
 
+for (x in v) {
+  print(x)
+  print('is in')
+  print(xml_attr(xml_parents(x)[2], "id"))
+}
 
