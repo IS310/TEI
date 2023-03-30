40526365a4cd910ea111b9d240281cbe12e5b097
JustinXre2020 <justinxiaoxulong@gmail.com>
2023-03-30 13:37:37 -0500

    Updated comments

--- TEIcommands.R
+++ TEIcommands.R
@@ -3,5 +3,8 @@ doc <- read_xml("JaneEyre.tei")
 xml_ns_strip(doc)
 doc %>% xml_find_all( '//*') %>% xml_path()
 
+#  Find verses from the whole book
 xml_find_all(doc, '//lg')
+
+#  Find verses from Chapter 24
 xml_find_all(doc, '//TEI//text//body//div//div[27]//lg')
