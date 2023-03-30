aa51e235532412cce16699f943f1821bcaebda27
JustinXre2020 <justinxiaoxulong@gmail.com>
2023-03-30 13:35:20 -0500

    Find verses from Chapter 24

--- TEIcommands.R
+++ TEIcommands.R
@@ -1,6 +1,7 @@
 library(xml2)
 doc <- read_xml("JaneEyre.tei")
 xml_ns_strip(doc)
-xml_find_all(doc, "//lg")
+doc %>% xml_find_all( '//*') %>% xml_path()
 
-
+xml_find_all(doc, '//lg')
+xml_find_all(doc, '//TEI//text//body//div//div[27]//lg')
