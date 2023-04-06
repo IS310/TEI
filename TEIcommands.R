36997fd156acec857d0a4bc9f96660737fe5a1e8
Dave Dubin <ddubin@illinois.edu>
2023-04-06 10:19:13 -0500

    Revert "Found the second parent of the node, which is the chapter, and found its id attribute."
    This reverts commit bb0caed8933491b7dc18c1f360bb94fa8b0e3ec6.

--- TEIcommands.R
+++ TEIcommands.R
@@ -1,11 +1,6 @@
 library(xml2)
 doc <- read_xml("JaneEyre.tei")
 xml_ns_strip(doc)
-v <- xml_find_all(doc, "//lg")
+xml_find_all(doc, "//lg")
 
-for (x in v) {
-  print(x)
-  print('is in')
-  print(xml_attr(xml_parents(x)[2], "id"))
-}
 
